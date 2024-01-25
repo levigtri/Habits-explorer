7f61757c8726c4ecac0a1a8a69f1ee143d805232
Levi Gomes <levigft@gmail.com>
2024-01-25 16:00:05 -0300

    add events, add logic, add save in storage

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
+// registros na memória
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-    run: ["01-01", "01-02", "01-06"],
+// registrou na memória um evento de click
+button.addEventListener("click", add)
+// sempre que o botão for clicado ele vai registrar a mudança com o save, para salvar a data no localstorage do navegador
+form.addEventListener("change", save)
+
+function add(){
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+    const dayExists = nlwSetup.dayExists(today)
+
+    if(dayExists){
+        alert("Dia já incluso! ❌")
+        return    
+    }
+    
+    alert("Dia adicionado com Sucesso! ✔")
+
+    nlwSetup.addDay(today)
 }
 
+// save transforma os dados em objetos para array de string
+function save(){
+    localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+}
+
+// transforma o array de string em objetos novamente, o OU é porquê na primeira vez o localstorage estará vazio
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
